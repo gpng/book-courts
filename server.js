20995f97b31ded03a57bc116e011df5eae105e30
Gerald <geraldpng@gmail.com>
2017-05-05 15:28:09 +0800

    initial commit

--- server.js
+++ server.js
@@ -1,15 +1,65 @@
 var express = require('express');
 var fs = require('fs');
-var request = require('request');
+var request = require('request').defaults({"jar": true});
 var cheerio = require('cheerio');
+var NodeRSA = require('node-rsa');
 var app = express();
 
-app.get('/scrape', function(req, res) {
+// store cookies to maintain session
 
-})
+// login
+function login() {
+	var url = 'https://members.myactivesg.com/auth';
+	request(url, function(error, response, html) {
+		if (!error) {
+			var $ = cheerio.load(html);
+			var email, ec_password, _csrf, rsapublickey, publickey;
 
-app.listen('8081');
+			// Load auth data
+			var auth = JSON.parse(fs.readFileSync('auth.json'));
 
-console.log('Magic happens on port 8081');
+			// get _csrf and rsa public key required for log-in
+			_csrf = $("input[name='_csrf']").val();
+			rsapublickey = $("input[name='rsapublickey']").val();
+			var key = new NodeRSA(rsapublickey);
+			ec_password = key.encrypt(auth.password, 'base64', 'base64');
 
-exports = module.exports = app;
+			// create login form data
+			var login_form = {
+				"email": auth.email,
+				"ec_password": ec_password,
+				"_csrf": _csrf
+			};
+
+			// create custom request headers
+			var login_header = {
+				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
+				"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+				"Content-Type": "application/x-www-form-urlencoded",
+				"Referer": "https://members.myactivesg.com/auth"
+			};
+
+			var login_url = "https://members.myactivesg.com/auth/signin";
+
+			request.post({"url": login_url, "headers": login_header, "form": login_form}, 
+				function(err, httpResponse, body) {
+					if (!error) {
+						url = 'https://members.myactivesg.com/profile';
+						request(url, function(error, response, html) {
+							console.log(html);
+						});
+					}
+				});
+		}
+	});
+}
+
+// select and checkout courts
+function add_available_courts() {
+	url = 'https://members.myactivesg.com/profile';
+	request(url, function(error, response, html) {
+		console.log(html);
+	});
+}
+
+login();
